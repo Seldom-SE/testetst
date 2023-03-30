e5a2426f547f0aac511d2e1934f221ab2d8b1037
Seldom-SE <38388947+Seldom-SE@users.noreply.github.com>
2023-03-30 10:07:51 -0700

    Polluted `Reflect` macro

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt::{Debug, Formatter, Result};
+
+use bevy::prelude::*;
+
+fn main() {}
+
+#[derive(Reflect)]
+struct MyStruct;
+
+struct MyError;
+
+impl Debug for MyError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "something occured")
+    }
 }
