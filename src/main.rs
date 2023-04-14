a4e96b9ec4c1468d528c452c70ff5c7d322bc0c5
Seldom-SE <38388947+Seldom-SE@users.noreply.github.com>
2023-04-14 10:43:45 -0700

    `chumsky` `.repeated().repeated()`

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
 use chumsky::prelude::*;
 
-struct Node(Vec<Node>);
+#[derive(Debug)]
+pub struct Values;
+
+pub fn parser<'a>() -> impl Parser<'a, &'a str, Values, extra::Err<Simple<'a, char>>> {
+    let comments = just("//").ignored().repeated();
+
+    any()
+        .ignored()
+        .padded_by(comments.repeated())
+        .repeated()
+        .collect::<Vec<_>>()
+        .map(|_| Values)
+}
 
 fn main() {
-    recursive(|node: Recursive<dyn Parser<&str, Node>>| {
-        node.separated_by(any()).collect().map(Node)
-    });
+    let src = "Would you rather have a CurseForge or a hex editor?";
+
+    println!("{src}");
+
+    println!("Creating parser...");
+    let parser = parser().then_ignore(end());
+    println!("Parsing file...");
+    let result = parser.parse(src);
+    println!("Done!");
+
+    if let Some(value) = result.output() {
+        println!("{value:?}");
+    }
+    for error in result.errors() {
+        println!("{error}");
+    }
 }
