40416a90cd22fe11444fe82cf0e98b776d2c30ad
Seldom-SE <38388947+Seldom-SE@users.noreply.github.com>
2023-11-14 09:24:22 -0700

    `seldom_pixel` animation test

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,59 @@
 use bevy::prelude::*;
-use bevy_xpbd_3d::prelude::*;
+use seldom_pixel::prelude::*;
+
+#[px_layer]
+struct Layer;
+
+fn animation_bundle(sprites: &mut PxAssets<PxSprite>) -> impl Bundle {
+    (
+        PxSpriteBundle::<Layer> {
+            sprite: sprites.load_animated("sprite.png", 8),
+            ..default()
+        },
+        PxAnimationBundle {
+            on_finish: PxAnimationFinishBehavior::Mark,
+            ..default()
+        },
+    )
+}
+
+fn init(mut commands: Commands, mut sprites: PxAssets<PxSprite>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((animation_bundle(&mut sprites), PxSubPosition::default()));
+}
+
+fn assign_animations(
+    mut commands: Commands,
+    mut sprites: PxAssets<PxSprite>,
+    animations: Query<Entity, With<PxAnimationFinished>>,
+    keys: Res<Input<KeyCode>>,
+) {
+    if !keys.just_pressed(KeyCode::Space) {
+        return;
+    }
+
+    for entity in &animations {
+        commands
+            .entity(entity)
+            .insert((animation_bundle(&mut sprites),))
+            .remove::<PxAnimationFinished>();
+    }
+}
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    resolution: Vec2::splat(512.).into(),
+                    ..default()
+                }),
+                ..default()
+            }),
+            PxPlugin::<Layer>::new(UVec2::splat(16), "palette.png".into()),
+        ))
+        .insert_resource(ClearColor(Color::BLACK))
         .add_systems(Startup, init)
+        .add_systems(Update, assign_animations)
         .run();
-}
-
-fn init(mut commands: Commands) {
-    for x in -16..16 {
-        for z in -8..8 {
-            for y in -8..0 {
-                commands.spawn((
-                    SpatialBundle {
-                        transform: Transform::from_xyz(x as f32, y as f32, z as f32),
-                        ..default()
-                    },
-                    RigidBody::Static,
-                    Collider::cuboid(1., 1., 1.),
-                ));
-            }
-        }
-    }
 }
