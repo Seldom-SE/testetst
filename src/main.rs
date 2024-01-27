22a5fa4aeafc454e82774a9e133844d83be3a11e
Seldom <38388947+Seldom-SE@users.noreply.github.com>
2024-01-27 16:38:50 -0700

    System initialize events

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,49 @@
 use bevy::prelude::*;
 
-fn detect(mut window_moved: EventReader<WindowMoved>) {
-    for _ in window_moved.read() {
-        info!("Moved!");
-    }
-}
-
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Update, detect)
+        .add_event::<MyEvent>()
+        .add_systems(Startup, init)
+        .add_systems(Update, (send, run_sys).chain())
         .run();
+}
+
+fn init(world: &mut World) {
+    let mut sys = IntoSystem::into_system(read);
+    sys.initialize(world);
+    world.insert_resource(Sys(Box::new(sys)));
+}
+
+fn send(mut write: EventWriter<MyEvent>, keys: Res<Input<KeyCode>>) {
+    if keys.just_pressed(KeyCode::Space) {
+        write.send(MyEvent);
+    }
+}
+
+#[derive(Resource)]
+struct Sys(Box<dyn System<In = (), Out = ()>>);
+
+fn run_sys(world: &mut World) {
+    // Temporarily taking ownership of `Sys`
+    let Sys(mut sys) = world.remove_resource().unwrap();
+    // sys.initialize(world);
+    // sys.set_last_run(world.change_tick());
+    sys.run((), world);
+    world.insert_resource(Sys(sys));
+}
+
+#[derive(Event)]
+struct MyEvent;
+
+fn read(mut has_run: Local<bool>, mut read: EventReader<MyEvent>) {
+    if !*has_run {
+        read.read().last();
+        *has_run = true;
+        return;
+    }
+
+    if read.read().last().is_some() {
+        info!("Read event");
+    }
 }
