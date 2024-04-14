94669703183d9f1b9c18504069e9c5039da7f0ac
Seldom <38388947+Seldom-SE@users.noreply.github.com>
2024-04-14 16:21:34 -0700

    Test if rapier lines up with bevy render

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,64 @@
-use bevy::{prelude::*, utils::Duration};
+use bevy::prelude::*;
+use bevy_rapier3d::{
+    geometry::Collider,
+    plugin::{NoUserData, RapierPhysicsPlugin},
+    render::RapierDebugRenderPlugin,
+};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Update, subtract_time)
+        .add_plugins((
+            DefaultPlugins,
+            RapierPhysicsPlugin::<NoUserData>::default(),
+            RapierDebugRenderPlugin::default(),
+        ))
+        .add_systems(Startup, init)
         .run();
 }
 
-fn subtract_time(time: Res<Time<Real>>) {
-    if let Some(last_update) = time.last_update() {
-        info!("{:?}", last_update - Duration::from_secs(60));
-    }
+fn init(mut cmd: Commands, mut meshes: ResMut<Assets<Mesh>>) {
+    cmd.spawn(Camera3dBundle {
+        transform: Transform::from_translation(Vec3::Y * 200.).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+
+    cmd.spawn(SpatialBundle::default()).with_children(|parent| {
+        parent
+            .spawn(SpatialBundle::default())
+            .with_children(|parent| {
+                parent
+                    .spawn(SpatialBundle::default())
+                    .with_children(|parent| {
+                        parent
+                            .spawn(SpatialBundle {
+                                transform: Transform::from_xyz(3., 0., 9.)
+                                    .with_scale(Vec3::new(16., 1., 1.)),
+                                ..default()
+                            })
+                            .with_children(|parent| {
+                                parent
+                                    .spawn(SpatialBundle {
+                                        transform: Transform::from_xyz(2.813, 0., 1.062)
+                                            .with_scale(Vec3::new(1., 1., 2.125)),
+                                        ..default()
+                                    })
+                                    .with_children(|parent| {
+                                        parent.spawn(SpatialBundle::default()).with_children(
+                                            |parent| {
+                                                parent.spawn((
+                                                    PbrBundle {
+                                                        mesh: meshes.add(Cuboid::from_size(
+                                                            Vec3::splat(1.),
+                                                        )),
+                                                        ..default()
+                                                    },
+                                                    Collider::cuboid(0.5, 0.5, 0.5),
+                                                ));
+                                            },
+                                        );
+                                    });
+                            });
+                    });
+            });
+    });
 }
