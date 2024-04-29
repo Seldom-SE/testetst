f33c19de2adf1d12cf0e8b2d8be7c3480eb7c863
Seldom <38388947+Seldom-SE@users.noreply.github.com>
2024-04-28 20:32:09 -0700

    Was figuring out a physics bug

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,34 @@
 use bevy::prelude::*;
-use bevy_rapier3d::{
-    geometry::Collider,
-    plugin::{NoUserData, RapierPhysicsPlugin},
-    render::RapierDebugRenderPlugin,
-};
+use bevy_editor_pls::prelude::*;
+use bevy_rapier3d::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins((
-            DefaultPlugins,
-            RapierPhysicsPlugin::<NoUserData>::default(),
-            RapierDebugRenderPlugin::default(),
-        ))
+        .add_plugins((DefaultPlugins, EditorPlugin::default()))
         .add_systems(Startup, init)
+        .add_systems(Update, insert_physics)
         .run();
 }
 
-fn init(mut cmd: Commands, mut meshes: ResMut<Assets<Mesh>>) {
+fn init(assets: Res<AssetServer>, mut cmd: Commands) {
     cmd.spawn(Camera3dBundle {
         transform: Transform::from_translation(Vec3::Y * 200.).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 
     cmd.spawn(SpatialBundle::default()).with_children(|parent| {
-        parent
-            .spawn(SpatialBundle::default())
-            .with_children(|parent| {
-                parent
-                    .spawn(SpatialBundle::default())
-                    .with_children(|parent| {
-                        parent
-                            .spawn(SpatialBundle {
-                                transform: Transform::from_xyz(3., 0., 9.)
-                                    .with_scale(Vec3::new(16., 1., 1.)),
-                                ..default()
-                            })
-                            .with_children(|parent| {
-                                parent
-                                    .spawn(SpatialBundle {
-                                        transform: Transform::from_xyz(2.813, 0., 1.062)
-                                            .with_scale(Vec3::new(1., 1., 2.125)),
-                                        ..default()
-                                    })
-                                    .with_children(|parent| {
-                                        parent.spawn(SpatialBundle::default()).with_children(
-                                            |parent| {
-                                                parent.spawn((
-                                                    PbrBundle {
-                                                        mesh: meshes.add(Cuboid::from_size(
-                                                            Vec3::splat(1.),
-                                                        )),
-                                                        ..default()
-                                                    },
-                                                    Collider::cuboid(0.5, 0.5, 0.5),
-                                                ));
-                                            },
-                                        );
-                                    });
-                            });
-                    });
-            });
+        parent.spawn(SceneBundle {
+            scene: assets.load("level.glb#Scene0"),
+            ..default()
+        });
+    });
+}
+
+fn insert_physics(names: Query<(&Name, Entity), Added<Name>>, mut cmd: Commands) {
+    names.iter().for_each(|(name, entity)| {
+        if &**name == "Plane" {
+            cmd.entity(entity)
+                .insert((RigidBody::Fixed, Collider::cuboid(1., 1., 1.)));
+        }
     });
 }
