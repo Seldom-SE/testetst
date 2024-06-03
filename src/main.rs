f4f71db7806360b1a7a9c43b4f0e8e290bb66b7a
Seldom <38388947+Seldom-SE@users.noreply.github.com>
2024-06-03 15:45:28 -0700

    `ImageToSprite` attempt

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,158 @@
 use bevy::{
-    ecs::{
-        query::QueryFilter,
-        system::{SystemParam, SystemParamItem},
-    },
     prelude::*,
+    render::{
+        render_resource::{
+            Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
+        },
+        view::RenderLayers,
+    },
 };
-
-fn check_system<Out, Func: Send + Sync + 'static, F0: SystemParam, F1: SystemParam>(_f: Func)
-where
-    for<'a> &'a mut Func:
-        FnMut(F0, F1) -> Out + FnMut(SystemParamItem<F0>, SystemParamItem<F1>) -> Out,
-    Out: 'static,
-{
-}
+use seldom_pixel::{prelude::*, sprite::ImageToSprite};
 
 fn main() {
-    check_system(align_to_screen::<(), &Transform>);
-    check_system(align_to_screen_2);
-
     App::new()
-        .add_systems(Update, align_to_screen::<(), &Transform>)
+        .add_plugins((
+            DefaultPlugins,
+            PxPlugin::<Layer>::new(UVec2::splat(512), "palette.palette.png"),
+        ))
+        .add_systems(Startup, setup)
+        .add_systems(Update, (rotator_system, list_all_colors))
         .run();
 }
 
-pub(crate) trait SystemGet<'a: 'w + 's, 'w, 's, M: 'static>: 'a + Sized {
-    type Param: SystemParam;
-    type Filter: QueryFilter;
+// Marks the first pass cube (rendered to a texture.)
+#[derive(Component)]
+struct FirstPassCube;
 
-    fn get(entity: Entity, param: &'a Self::Param) -> Option<Self>;
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut images: ResMut<Assets<Image>>,
+) {
+    let size = Extent3d {
+        width: 512,
+        height: 512,
+        ..default()
+    };
+
+    // This is the texture that will be rendered to.
+    let mut image = Image {
+        texture_descriptor: TextureDescriptor {
+            label: None,
+            size,
+            dimension: TextureDimension::D2,
+            format: TextureFormat::Bgra8UnormSrgb,
+            mip_level_count: 1,
+            sample_count: 1,
+            usage: TextureUsages::TEXTURE_BINDING
+                | TextureUsages::COPY_DST
+                | TextureUsages::RENDER_ATTACHMENT,
+            view_formats: &[],
+        },
+        ..default()
+    };
+
+    // fill image.data with zeroes
+    image.resize(size);
+
+    let image_handle = images.add(image);
+
+    let cube_handle = meshes.add(Cuboid::new(4.0, 4.0, 4.0));
+    let cube_material_handle = materials.add(StandardMaterial {
+        base_color: Color::rgb(0.8, 0.7, 0.6),
+        reflectance: 0.02,
+        unlit: false,
+        ..default()
+    });
+
+    // This specifies the layer used for the first pass, which will be attached to the first pass camera and cube.
+    let first_pass_layer = RenderLayers::layer(1);
+
+    // The cube that will be rendered to the texture.
+    commands.spawn((
+        PbrBundle {
+            mesh: cube_handle,
+            material: cube_material_handle,
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 1.0)),
+            ..default()
+        },
+        FirstPassCube,
+        first_pass_layer,
+    ));
+
+    // Light
+    // NOTE: we add the light to all layers so it affects both the rendered-to-texture cube, and the cube on which we display the texture
+    // Setting the layer to RenderLayers::layer(0) would cause the main view to be lit, but the rendered-to-texture cube to be unlit.
+    // Setting the layer to RenderLayers::layer(1) would cause the rendered-to-texture cube to be lit, but the main view to be unlit.
+    commands.spawn((
+        PointLightBundle {
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 10.0)),
+            ..default()
+        },
+        RenderLayers::all(),
+    ));
+
+    commands.spawn((
+        Camera3dBundle {
+            camera: Camera {
+                // render before the "main pass" camera
+                order: -1,
+                target: image_handle.clone().into(),
+                clear_color: Color::WHITE.into(),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 15.0))
+                .looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        first_pass_layer,
+    ));
+
+    commands.spawn(SpriteBundle {
+        texture: image_handle.clone(),
+        transform: Transform::from_translation(Vec3::Z),
+        ..default()
+    });
+
+    commands.spawn((
+        PxSpriteBundle::<Layer> {
+            anchor: PxAnchor::BottomLeft,
+            ..default()
+        },
+        ImageToSprite(image_handle.clone()),
+    ));
+
+    // The main pass camera.
+    commands.spawn(Camera2dBundle::default());
+    commands.insert_resource(MyImage(image_handle));
 }
 
-impl<'a: 'w + 's, 'w, 's, T: Component> SystemGet<'a, 'w, 's, ()> for &'a T {
-    type Param = Query<'w, 's, &'static T>;
-    type Filter = With<T>;
-
-    fn get(entity: Entity, param: &'a Self::Param) -> Option<Self> {
-        Some(param.get(entity).unwrap())
+/// Rotates the inner cube (first pass)
+fn rotator_system(time: Res<Time>, mut query: Query<&mut Transform, With<FirstPassCube>>) {
+    for mut transform in &mut query {
+        transform.rotate_x(1.5 * time.delta_seconds());
+        transform.rotate_z(1.3 * time.delta_seconds());
     }
 }
 
-fn align_to_screen<M, T>(
-    ts: Query<Entity, <T as SystemGet<'_, '_, '_, M>>::Filter>,
-    param: <T as SystemGet<'_, '_, '_, M>>::Param,
-) where
-    for<'a: 'w + 's, 'w, 's> T: SystemGet<'a, 'w, 's, M>,
-{
-    for t in &ts {
-        if T::get(t, &param).is_some() {
-            info!("Wow, I got a `T`")
+#[derive(Resource)]
+struct MyImage(Handle<Image>);
+
+fn list_all_colors(my_image: Res<MyImage>, images: Res<Assets<Image>>) {
+    info!("listing all colors");
+
+    let MyImage(ref image_handle) = *my_image;
+    for pixel in images.get(image_handle).unwrap().data.chunks_exact(4) {
+        if pixel != [0, 0, 0, 0] {
+            let &[blue, green, red, alpha] = pixel else {
+                panic!();
+            };
+
+            info!("found a color: R{red} G{green} B{blue} A{alpha}");
         }
     }
 }
 
-fn align_to_screen_2<'a, 'w, 's>(
-    _ts: Query<Entity, With<Transform>>,
-    _param: Query<'w, 's, &'static Transform>,
-) {
-}
-
-// pub(crate) trait SystemGet<'a, M>: 'a + Sized {
-//     type Param<'w, 's>: SystemParam
-//     where
-//         'w: 'a,
-//         's: 'a;
-//     type Filter: QueryFilter;
-//
-//     fn get<'w: 'a, 's: 'a>(entity: Entity, param: &'a Self::Param<'w, 's>) -> Option<Self>;
-// }
-//
-// impl<'a, T: Component> SystemGet<'a, ()> for &'a T {
-//     type Param<'w, 's> = Query<'w, 's, &'static T> where 'w: 'a, 's: 'a;
-//     type Filter = With<T>;
-//
-//     fn get<'w: 'a, 's: 'a>(entity: Entity, param: &'a Self::Param<'w, 's>) -> Option<Self> {
-//         Some(param.get(entity).unwrap())
-//     }
-// }
-
-// pub(crate) trait SystemGet<'a, 'w, 's, M: 'static>: 'a + Sized {
-//     type Param: SystemParam;
-//     type Filter: QueryFilter;
-//
-//     fn get(entity: Entity, param: &'a Self::Param) -> Option<Self>;
-// }
-//
-// impl<'a, 'w, 's, T: Component> SystemGet<'a, 'w, 's, ()> for &'a T {
-//     type Param = Query<'w, 's, &'static T>;
-//     type Filter = With<T>;
-//
-//     fn get(entity: Entity, param: &'a Self::Param) -> Option<Self> {
-//         Some(param.get(entity).unwrap())
-//     }
-// }
+#[px_layer]
+struct Layer;
