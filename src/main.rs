aaac435fe295f101a4c879c94edb796fb7c31b6f
Seldom <38388947+Seldom-SE@users.noreply.github.com>
2024-07-09 10:32:08 -0700

    `AlignContent::Center` doesn't seem to work

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,32 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(AssetPlugin {
-            mode: AssetMode::Processed,
-            ..default()
-        }))
+        .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
         .run();
 }
 
-fn setup(mut commands: Commands, assets: Res<AssetServer>) {
-    commands.spawn(SceneBundle {
-        scene: assets.load("fireminion.gltf#Scene0"),
-        ..default()
-    });
+fn setup(mut cmds: Commands) {
+    cmds.spawn(Camera2dBundle::default());
 
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 7., 14.).looking_at(Vec3::ZERO, Vec3::Y),
+    cmds.spawn(NodeBundle {
+        style: Style {
+            align_content: AlignContent::Center,
+            min_width: Val::Percent(100.),
+            min_height: Val::Percent(100.),
+            ..default()
+        },
         ..default()
+    })
+    .with_children(|root| {
+        root.spawn(NodeBundle {
+            style: Style {
+                width: Val::Px(50.),
+                height: Val::Px(50.),
+                ..default()
+            },
+            background_color: Color::WHITE.into(),
+            ..default()
+        });
     });
 }
