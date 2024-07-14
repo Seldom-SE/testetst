b70266e9f3779c4c4858a5a449cfe6b2b2d06278
Seldom <38388947+Seldom-SE@users.noreply.github.com>
2024-07-14 12:33:49 -0700

    Compressed image panic

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,18 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(AssetPlugin {
+            mode: AssetMode::Processed,
+            ..default()
+        }))
         .add_systems(Startup, setup)
         .run();
 }
 
-fn setup(mut cmds: Commands) {
-    cmds.spawn(Camera2dBundle::default());
+#[derive(Resource)]
+#[allow(dead_code)]
+struct MyHandle(Handle<Image>);
 
-    cmds.spawn(NodeBundle {
-        style: Style {
-            align_content: AlignContent::Center,
-            min_width: Val::Percent(100.),
-            min_height: Val::Percent(100.),
-            ..default()
-        },
-        ..default()
-    })
-    .with_children(|root| {
-        root.spawn(NodeBundle {
-            style: Style {
-                width: Val::Px(50.),
-                height: Val::Px(50.),
-                ..default()
-            },
-            background_color: Color::WHITE.into(),
-            ..default()
-        });
-    });
+fn setup(mut cmds: Commands, assets: Res<AssetServer>) {
+    cmds.insert_resource(MyHandle(assets.load::<Image>("square.png")));
 }
