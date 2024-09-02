dae05ecb524069883a22cb3d0e1b7415994357bc
Seldom <38388947+Seldom-SE@users.noreply.github.com>
2024-09-02 10:41:16 -0700

    9-slice seams repro attempt

--- copy_assets.sh
+++
@@ -1,4 +0,0 @@
-for i in $(seq 1 127);
-do
-    cp "assets/0.png" "assets/$i.png"
-done
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,14 @@
 #![allow(clippy::type_complexity)]
 
-use bevy::{asset::LoadState, prelude::*};
+use bevy::prelude::*;
 
 // the `bevy_main` proc_macro generates the required boilerplate for iOS and Android
 #[bevy_main]
 pub fn main() {
     let mut app = App::new();
-    app.add_plugins(DefaultPlugins.set(AssetPlugin {
-        mode: AssetMode::Processed,
-        ..default()
-    }))
-    .add_systems(Startup, setup_scene)
-    .add_systems(Update, check_loaded)
-    .run();
+    app.add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup_scene)
+        .run();
 }
 
 #[allow(dead_code)]
@@ -20,34 +16,22 @@ pub fn main() {
 struct SuperImportantImages(Vec<Handle<Image>>);
 
 fn setup_scene(mut commands: Commands, assets: Res<AssetServer>) {
-    commands.insert_resource(SuperImportantImages(
-        (0..128)
-            .map(|i| assets.load(format!("{}.png", i)))
-            .collect(),
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn((
+        ImageScaleMode::Sliced(TextureSlicer {
+            border: BorderRect::square(16.),
+            sides_scale_mode: SliceScaleMode::Tile { stretch_value: 1. },
+            ..default()
+        }),
+        ImageBundle {
+            image: assets.load("9slice.png").into(),
+            style: Style {
+                width: Val::Percent(100.),
+                height: Val::Percent(100.),
+                ..default()
+            },
+            ..default()
+        },
     ));
-}
-
-fn check_loaded(handles: Res<SuperImportantImages>, assets: Res<AssetServer>) {
-    let mut no_state = 0;
-    let mut not_loaded = 0;
-    let mut loading = 0;
-    let mut loaded = 0;
-    let mut failed = 0;
-
-    for handle in &**handles {
-        *match assets.get_load_state(handle) {
-            None => &mut no_state,
-            Some(LoadState::NotLoaded) => &mut not_loaded,
-            Some(LoadState::Loading) => &mut loading,
-            Some(LoadState::Loaded) => &mut loaded,
-            Some(LoadState::Failed(_)) => &mut failed,
-        } += 1;
-    }
-
-    info!("== LOAD STATES ==");
-    info!("no_state   {no_state}");
-    info!("not_loaded {not_loaded}");
-    info!("loading    {loading}");
-    info!("loaded     {loaded}");
-    info!("failed     {failed}");
 }
