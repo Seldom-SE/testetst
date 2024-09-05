c24ae75c330862b8ed59b7b0728b38e1daf34075
Seldom <38388947+Seldom-SE@users.noreply.github.com>
2024-09-05 12:04:15 -0700

    Very minimal repro

--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,40 @@
-use std::f32::consts::TAU;
-
-use bevy::{color::palettes::css::MAGENTA, prelude::*};
+use bevy::prelude::*;
 use bevy_vector_shapes::prelude::*;
 
 pub fn main() {
     App::new()
         .add_plugins((DefaultPlugins, ShapePlugin::default()))
         .add_systems(Startup, init)
-        .add_systems(Update, (rotate_camera, draw_shapes))
+        .add_systems(Update, draw_shapes)
         .run();
 }
 
-const CAMERA_CIRCLE_RADIUS: f32 = 100.;
-const CAMERA_HEIGHT: f32 = 700.;
-const CAMERA_FOCUS_HEIGHT: f32 = 430.;
-
-fn camera_angle_to_tsf(angle: f32) -> Transform {
-    let camera_plane_point = Vec2::from_angle(angle) * CAMERA_CIRCLE_RADIUS;
-    Transform::from_xyz(camera_plane_point.x, CAMERA_HEIGHT, camera_plane_point.y)
-        .looking_at(Vec3::Y * CAMERA_FOCUS_HEIGHT, Vec3::Y)
-}
+const CAM_POS: Vec3 = Vec3::new(0., 70., -10.);
+const BACKGROUND_POS: Vec3 = Vec3::new(0., 0., 10.);
 
 fn init(mut cmds: Commands) {
-    cmds.spawn((
-        CamAngle(0.),
-        Camera3dBundle {
-            transform: camera_angle_to_tsf(0.),
-            ..default()
-        },
-    ));
-
-    for i in 0..16 {
-        let plane_point = Vec2::from_angle(i as f32 / 16. * TAU) * 200.;
-        cmds.spawn(DrawShape {
-            pos: Vec3::new(plane_point.x, 48.5, plane_point.y),
-            log: i == 4,
-        });
-    }
+    cmds.spawn(Camera3dBundle {
+        transform: Transform::from_translation(CAM_POS).looking_at(BACKGROUND_POS, Dir3::Y),
+        ..default()
+    });
 }
 
-#[derive(Component, Deref, DerefMut)]
-struct CamAngle(f32);
-
-fn rotate_camera(mut cameras_query: Query<(&mut CamAngle, &mut Transform)>, time: Res<Time>) {
-    let (mut angle, mut tsf) = cameras_query.single_mut();
-    **angle += time.delta_seconds() * 0.5;
-    *tsf = camera_angle_to_tsf(**angle);
-}
-
-#[derive(Component)]
-struct DrawShape {
-    pos: Vec3,
-    log: bool,
-}
-
-fn draw_shapes(
-    shapes_query: Query<&DrawShape>,
-    cameras_query: Query<&GlobalTransform, With<Camera>>,
-    mut painter: ShapePainter,
-) {
-    let cam_pos = cameras_query.single().translation();
-
+fn draw_shapes(mut painter: ShapePainter) {
     painter.set_3d();
 
-    painter.alignment = Alignment::Billboard;
-    for v in [Vec3::X, Vec3::Z] {
-        painter.color = MAGENTA.into();
-        painter.line(v * 1000. + Vec3::Y * 50., v * -1000. + Vec3::Y * 50.);
-    }
-    painter.alignment = Alignment::Flat;
+    painter.color = Color::BLACK;
+    // Look away from the camera so positive X is to the right
+    painter.transform =
+        Transform::from_translation(BACKGROUND_POS).looking_to(BACKGROUND_POS - CAM_POS, Dir3::Y);
 
-    for &DrawShape { pos, log } in &shapes_query {
-        painter.color = Color::BLACK;
-        // The direction in which to layer the shapes
-        let layer_dir = (cam_pos - pos).normalize_or_zero();
-        // Look away from the camera so positive X is to the right
-        painter.transform = Transform::from_translation(pos).looking_to(-layer_dir, Dir3::Y);
+    let width = 10.;
+    let height = 1.;
 
-        let width = 10.;
-        let height = 1.;
-        let right = width / 2.;
-        let left = -right;
+    // Background
+    painter.rect(Vec2::new(width, height));
 
-        // Background
-        painter.rect(Vec2::new(width, height));
-
-        // Health
-        painter.color = if log { Srgba::GREEN } else { Srgba::RED }.into();
-        painter.translate(Vec3::X * left * 0.5);
-        // Draw this shape closer to the camera. The 50 makes sure this shape is well in front of
-        // the previous.
-        painter.transform.translation += layer_dir * 50.;
-        painter.rect(Vec2::new(width / 2., height));
-
-        if log {
-            info!(
-                "back: {pos}, health: {}, cam: {cam_pos}, layer_dir: {layer_dir}",
-                painter.transform.translation
-            );
-        }
-    }
+    // Health
+    painter.color = Srgba::RED.into();
+    painter.set_translation((CAM_POS + BACKGROUND_POS) / 2.);
+    painter.rect(Vec2::new(width, height));
 }
